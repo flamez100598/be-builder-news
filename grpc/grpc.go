c4ca570acec084c71e8edb8e6850b9aa084d9d86
tungps <tung.pham@metub.net>
2023-04-20 16:59:19 +0700

    chore(contentJp): grpc

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -1,13 +1,14 @@
 package appgrpc
 
 import (
-	"google.golang.org/grpc"
-	pb "lykafe/news/grpc/news/pb"
 	"lykafe/news/grpc/handler"
+	pb "lykafe/news/grpc/news/pb"
+
+	"google.golang.org/grpc"
 )
 
 func NewGrpcServer(newsHandler *handler.NewsHandler,
-	subscriptionHandler *handler.SubscriptionHandler) *grpc.Server{
+	subscriptionHandler *handler.SubscriptionHandler) *grpc.Server {
 	s := grpc.NewServer()
 	pb.RegisterNewsServiceServer(s, newsHandler)
 	pb.RegisterSubscriptionServiceServer(s, subscriptionHandler)
--- grpc/mapper/news_mapper.go
+++ grpc/mapper/news_mapper.go
@@ -1,9 +1,9 @@
 package mapper
 
 import (
-	"lykafe/news/grpc/news/pb"
 	"lykafe/news/core/data/dto"
 	"lykafe/news/core/data/model"
+	"lykafe/news/grpc/news/pb"
 	"time"
 	// "fmt"
 )
@@ -13,49 +13,52 @@ func Pb2DtoPublishNewsReq(req *pb.PublishNewsReq) *dto.PublishNewsReq {
 		return nil
 	}
 	publishAt, _ := time.Parse("2006-01-02 15:04:05", req.PublishAt)
-	return &dto.PublishNewsReq {
-		Id: req.Id,
-		Title: req.Title,
-		Content: req.Content,
-		Description: req.Description,
-		ImgUrl: req.ImgUrl,
-		Slug: req.Slug,
-		MetaKw: req.MetaKw,
-		MetaDesc: req.MetaDesc,
-		IsFeatured: req.IsFeatured,
-		IsHot: req.IsHot,
-		IsTrending: req.IsTrending,
-		Tags: req.Tags,
-		Related: req.Related,
-		Category: int(req.Category),
-		SubCategory: int(req.SubCategory),
-		PublishBy: req.PublishBy,
-		PublishAt: publishAt,
-		AuthorId: req.AuthorId,
+	return &dto.PublishNewsReq{
+		Id:            req.Id,
+		Title:         req.Title,
+		Content:       req.Content,
+		ContentJp:     req.ContentJp,
+		Description:   req.Description,
+		DescriptionJp: req.DescriptionJp,
+		ImgUrl:        req.ImgUrl,
+		Slug:          req.Slug,
+		MetaKw:        req.MetaKw,
+		MetaDesc:      req.MetaDesc,
+		IsFeatured:    req.IsFeatured,
+		IsHot:         req.IsHot,
+		IsTrending:    req.IsTrending,
+		Tags:          req.Tags,
+		Related:       req.Related,
+		Category:      int(req.Category),
+		SubCategory:   int(req.SubCategory),
+		PublishBy:     req.PublishBy,
+		PublishAt:     publishAt,
+		AuthorId:      req.AuthorId,
 	}
 }
-
 
 func Model2PbNews(news *model.News) *pb.News {
 	if news == nil {
 		return nil
 	}
-	return &pb.News {
-		Title: news.Title,
-		Content: news.Content,
-		Description: news.Description,
-		ImgUrl: news.ImgUrl,
-		MetaKw: news.MetaKw,
-		MetaDesc: news.MetaDesc,
-		Slug: "",
+	return &pb.News{
+		Title:         news.Title,
+		Content:       news.Content,
+		ContentJp:     news.ContentJp,
+		Description:   news.Description,
+		DescriptionJp: news.DescriptionJp,
+		ImgUrl:        news.ImgUrl,
+		MetaKw:        news.MetaKw,
+		MetaDesc:      news.MetaDesc,
+		Slug:          "",
 		// IsFeatured: news.IsFeatured,
 		// IsHot: news.IsHot,
 		// IsTrending: news.IsTrending,
 		// Tags: news.Tags,
 		// Related: news.Related,
-		Category: int32(news.Category),
+		Category:    int32(news.Category),
 		SubCategory: int32(news.SubCategory),
-		PublishBy: news.PublishBy,
+		PublishBy:   news.PublishBy,
 	}
 }
 
@@ -63,33 +66,35 @@ func Model2PbNewsView(news *model.NewsView) *pb.NewsView {
 	if news == nil {
 		return nil
 	}
-	return &pb.NewsView {
-		Id: news.Id,
-		Title: news.Title,
-		Content: news.Content,
-		Description: news.Description,
-		ImgUrl: news.ImgUrl,
-		MetaKw: news.MetaKw,
-		MetaDesc: news.MetaDesc,
-		Slug: news.Slug,
-		Category: int32(news.Category),
-		SubCategory: int32(news.SubCategory),
-		CommentNum: int32(news.CommentNum),
-		VoteNum: int32(news.VoteNum),
-		ViewNum: int32(news.ViewNum),
-		Status: int32(news.Status),
-		PublishBy: news.PublishBy,
-		Ranking: news.Ranking,
-		CreatedAt: news.CreatedAt.Format("2006-01-02 15:04:05"),
-		UpdatedAt: news.UpdatedAt.Format("2006-01-02 15:04:05"),
-		PublishedAt: news.PublishedAt.Format("2006-01-02 15:04:05"),
-		Order: int32(news.Order),
+	return &pb.NewsView{
+		Id:                news.Id,
+		Title:             news.Title,
+		Content:           news.Content,
+		ContentJp:         news.ContentJp,
+		Description:       news.Description,
+		DescriptionJp:     news.DescriptionJp,
+		ImgUrl:            news.ImgUrl,
+		MetaKw:            news.MetaKw,
+		MetaDesc:          news.MetaDesc,
+		Slug:              news.Slug,
+		Category:          int32(news.Category),
+		SubCategory:       int32(news.SubCategory),
+		CommentNum:        int32(news.CommentNum),
+		VoteNum:           int32(news.VoteNum),
+		ViewNum:           int32(news.ViewNum),
+		Status:            int32(news.Status),
+		PublishBy:         news.PublishBy,
+		Ranking:           news.Ranking,
+		CreatedAt:         news.CreatedAt.Format("2006-01-02 15:04:05"),
+		UpdatedAt:         news.UpdatedAt.Format("2006-01-02 15:04:05"),
+		PublishedAt:       news.PublishedAt.Format("2006-01-02 15:04:05"),
+		Order:             int32(news.Order),
 		UsernamePublishBy: news.UsernamePublishBy,
-		NamePublishBy: news.NamePublishBy,
-		AvatarPublishBy: news.AvatarPublishBy,
-		UsernameUpdateBy: news.UsernameUpdateBy,
-		NameUpdateBy: news.NameUpdateBy,
-		AvatarUpdateBy: news.AvatarUpdateBy,
+		NamePublishBy:     news.NamePublishBy,
+		AvatarPublishBy:   news.AvatarPublishBy,
+		UsernameUpdateBy:  news.UsernameUpdateBy,
+		NameUpdateBy:      news.NameUpdateBy,
+		AvatarUpdateBy:    news.AvatarUpdateBy,
 	}
 }
 
@@ -99,19 +104,19 @@ func Pb2DtoAdminSearchNewsReq(req *pb.AdminSearchNewsReq) *dto.AdminSearchNewsRe
 	}
 	from, _ := time.Parse("2006-01-02 15:04:05", req.From)
 	to, _ := time.Parse("2006-01-02 15:04:05", req.To)
-	return &dto.AdminSearchNewsReq {
-		Title: req.Title,
-		Content: req.Content,
-		Category: int(req.Category),
+	return &dto.AdminSearchNewsReq{
+		Title:       req.Title,
+		Content:     req.Content,
+		Category:    int(req.Category),
 		SubCategory: int(req.SubCategory),
 		Description: req.Description,
-		PublishBy: req.PublishBy,
-		UpdateBy: req.UpdateBy,
-		From: from,
-		To: to,
-		Status: int(req.Status),
-		Offset: int(req.Offset),
-		Limit: int(req.Limit),
+		PublishBy:   req.PublishBy,
+		UpdateBy:    req.UpdateBy,
+		From:        from,
+		To:          to,
+		Status:      int(req.Status),
+		Offset:      int(req.Offset),
+		Limit:       int(req.Limit),
 	}
 }
 
@@ -121,17 +126,17 @@ func Pb2DtoAdminSearchRelatedNewsReq(req *pb.AdminSearchRelatedNewsReq) *dto.Adm
 	}
 	from, _ := time.Parse("2006-01-02 15:04:05", req.From)
 	to, _ := time.Parse("2006-01-02 15:04:05", req.To)
-	return &dto.AdminSearchRelatedNewsReq {
-		Tags: req.Tags,
-		Title: req.Title,
-		Content: req.Content,
-		Category: int(req.Category),
+	return &dto.AdminSearchRelatedNewsReq{
+		Tags:        req.Tags,
+		Title:       req.Title,
+		Content:     req.Content,
+		Category:    int(req.Category),
 		SubCategory: int(req.SubCategory),
-		PublishBy: req.PublishBy,
-		From: from,
-		To: to,
-		Offset: int(req.Offset),
-		Limit: int(req.Limit),
+		PublishBy:   req.PublishBy,
+		From:        from,
+		To:          to,
+		Offset:      int(req.Offset),
+		Limit:       int(req.Limit),
 	}
 }
 
@@ -139,9 +144,9 @@ func Pb2DtoSearchNewsReq(req *pb.SearchNewsReq) *dto.SearchNewsReq {
 	if req == nil {
 		return nil
 	}
-	return &dto.SearchNewsReq {
+	return &dto.SearchNewsReq{
 		Keywords: req.Keywords,
-		Offset: int(req.Offset),
-		Limit: int(req.Limit),
+		Offset:   int(req.Offset),
+		Limit:    int(req.Limit),
 	}
 }
